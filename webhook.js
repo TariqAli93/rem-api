1ad9e6f6ee988fa4d7522c244c3e4021abc8a7dc
TariqAli93 <tariqali9300@gmail.com>
2025-07-23 14:39:12 +0300

    edit config & index.js

--- webhook.js
+++ webhook.js
@@ -7,6 +7,9 @@ app.use(express.json());
 
 app.post("/webhook", (req, res) => {
     // أمان: تأكد أن البوش من GitHub
+    if (!req.body || !req.body.ref) {
+        return res.status(400).send("Bad Request: Missing ref in body");
+    }
     const branch = req.body.ref;
     if (branch !== "refs/heads/main") return res.sendStatus(200);
 
